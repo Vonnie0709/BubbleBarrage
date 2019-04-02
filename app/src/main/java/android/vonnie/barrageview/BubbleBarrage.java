5c8636b83408ec89fda819d9251c4721c4248ca3
邹龙平 <zoulongping@lobochat.com>
2019-04-02 10:52:56 +0800

    init project

---
+++ app/src/main/java/android/vonnie/barrageview/BubbleBarrage.java
@@ -0,0 +1,356 @@
+package android.vonnie.barrageview;
+
+import android.animation.Animator;
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author LongpingZou
+ * @date 2019/3/29
+ */
+public class BubbleBarrage {
+    private final static String TAG = "BubbleBarrage";
+    private LayoutTransition transition = new LayoutTransition();
+    private Context context;
+    private LinearLayout barrageContainer;
+    private int layout;
+    private int index = 0;
+    /**
+     * 执行间隔
+     */
+    private long intervalTime = 2000;
+    /**
+     * 显示的数量
+     */
+    private int visibleCount = 3;
+    /**
+     * 每个弹幕要展示的时间=推送间隔*要显示的个数
+     */
+    private long visibleTime = intervalTime * visibleCount;
+
+    private DisplayMetrics metrics;
+    private Handler handler = new Handler();
+    /**
+     * barrage load listener
+     */
+    private OnBarrageLoadListener onBarrageLoadListener;
+    private int margin = 5;
+    private List<String> barrages = new ArrayList<>();
+
+    /**
+     * initialize bubble barrage
+     *
+     * @param context
+     * @param container bubble barrage container  {@link LinearLayout}
+     * @param layout    item layout res id
+     * @return
+     */
+    public BubbleBarrage init(Context context, LinearLayout container, int layout) {
+        this.context = context;
+        this.layout = layout;
+        this.barrageContainer = container;
+        container.setLayoutTransition(transition);
+        setAppearTransition();
+        setDisappearTransition();
+        return this;
+    }
+
+    /**
+     * setting interval time
+     *
+     * @param intervalTime interval time
+     */
+    public BubbleBarrage setIntervalTime(int intervalTime) {
+        this.intervalTime = intervalTime;
+        visibleTime = intervalTime * visibleCount;
+        return this;
+    }
+
+    /**
+     * setting visible counts
+     *
+     * @param count visible counts
+     * @return
+     */
+    public BubbleBarrage setVisibleCount(int count) {
+        this.visibleCount = count;
+        visibleTime = intervalTime * visibleCount;
+        return this;
+    }
+
+    /**
+     * setting margins
+     *
+     * @param margin dp
+     */
+    public BubbleBarrage setItemMargin(int margin) {
+        this.margin = (int) parseToDp(margin);
+        return this;
+    }
+
+
+    /**
+     * set barrage load listener
+     *
+     * @param onBarrageLoadListener
+     */
+    public BubbleBarrage setOnBarrageLoadListener(OnBarrageLoadListener onBarrageLoadListener) {
+        this.onBarrageLoadListener = onBarrageLoadListener;
+        return this;
+    }
+
+    /**
+     * start to play barrage
+     *
+     * @param stringList
+     * @param delay
+     */
+    public BubbleBarrage start(final List<String> stringList, int delay) {
+        if (stringList == null) {
+            try {
+                throw new Exception("barrage data should not be null");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return this;
+        }
+        barrages.clear();
+        barrages.addAll(stringList);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.e("ABC", "current in index -->" + index + "barrage size -->" + barrages.size());
+                if (index > barrages.size() - 1) {
+                    index = 0;
+                    barrages.clear();
+                    handler.postDelayed(this, intervalTime);
+                } else {
+                    addBarrageToContainer(index);
+                    index++;
+                    handler.postDelayed(this, intervalTime);
+                }
+                Log.i("ABC", "current index -->" + index);
+            }
+        };
+
+        handler.postDelayed(runnable, delay);
+        return this;
+    }
+
+
+    /**
+     * insert a barrage to list for next playing
+     *
+     * @param barrage
+     */
+    public BubbleBarrage insertToNext(String barrage) {
+        barrages.add(index, barrage);
+        Log.i("ABC", "barrages size -->" + barrages.size());
+        return this;
+    }
+
+
+    /**
+     * append barrages to list
+     *
+     * @param barrages
+     */
+    public BubbleBarrage appendBarrages(List<String> barrages) {
+        this.barrages.addAll(barrages);
+        return this;
+    }
+
+    /**
+     * append a barrage to list
+     *
+     * @param barrage
+     */
+    public BubbleBarrage appendBarrage(String barrage) {
+        barrages.add(barrage);
+        return this;
+    }
+
+    /**
+     * set transition of view appearing
+     */
+    @SuppressLint("ObjectAnimatorBinding")
+    private void setAppearTransition() {
+        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0f, 1f);
+        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 0f, 1f);
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 0f, 1f);
+        ObjectAnimator multiAnim = ObjectAnimator.ofPropertyValuesHolder(this, alpha, scaleX, scaleY).setDuration(transition.getDuration(LayoutTransition.APPEARING));
+        multiAnim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                View view = (View) ((ObjectAnimator) animation).getTarget();
+                if (view != null) {
+                    view.setPivotX(0f);
+                    view.setPivotY(view.getHeight());
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        transition.setAnimator(LayoutTransition.APPEARING, multiAnim);
+
+    }
+
+
+    /**
+     * set transition of view disappearing
+     */
+    @SuppressLint("ObjectAnimatorBinding")
+    private void setDisappearTransition() {
+        PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 1f, 0f);
+        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 0f);
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", 1f, 0f);
+        ObjectAnimator multiAnim = ObjectAnimator.ofPropertyValuesHolder(this, alpha, scaleX, scaleY).setDuration(transition.getDuration(LayoutTransition.DISAPPEARING));
+        multiAnim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                View view = (View) ((ObjectAnimator) animation).getTarget();
+                if (view != null) {
+                    view.setPivotX(0f);
+                    view.setPivotY(0f);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        transition.setAnimator(LayoutTransition.DISAPPEARING, multiAnim);
+    }
+
+    /**
+     * add barrage into container
+     */
+    private void addBarrageToContainer(int index) {
+        //create barrage view
+        View barrage = createBarrage(index);
+        //setting each barrage margins
+        LinearLayout.LayoutParams layoutParams = setItemMargin();
+        barrageContainer.addView(barrage, layoutParams);
+        //at the end of visible time, remove view
+        barrage.postDelayed(removeView(barrage), visibleTime);
+    }
+
+    /**
+     * create bubble barrage
+     *
+     * @return
+     */
+    private View createBarrage(int index) {
+        View barrage = LayoutInflater.from(context).inflate(layout, null);
+        //setting barrages load listener
+        if (onBarrageLoadListener != null) {
+            onBarrageLoadListener.loadBarrage(barrage, barrages, index);
+        }
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+        alphaAnimation.setDuration(visibleTime + 1000);
+        alphaAnimation.setFillAfter(true);
+        barrage.startAnimation(alphaAnimation);
+        return barrage;
+    }
+
+
+    /**
+     * setting barrages margin
+     *
+     * @return
+     */
+    private LinearLayout.LayoutParams setItemMargin() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(margin, 0, margin, margin);
+        return layoutParams;
+    }
+
+    /**
+     * at the end of visible time,remove barrage view
+     *
+     * @param barrage
+     * @return
+     */
+    private Runnable removeView(final View barrage) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                barrageContainer.removeView(barrage);
+            }
+        };
+    }
+
+
+    /**
+     * parse px to dp
+     *
+     * @param value
+     * @return
+     */
+    private float parseToDp(int value) {
+        if (metrics == null) {
+            metrics = new DisplayMetrics();
+            try {
+                ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+        }
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, metrics);
+    }
+
+    /**
+     * when create barrage, call this listener
+     */
+    interface OnBarrageLoadListener {
+        /**
+         * load barrage
+         *
+         * @param view  item view
+         * @param data  data array
+         * @param index barrage index
+         */
+        void loadBarrage(View view, List<String> data, int index);
+    }
+}
---
+++ app/src/main/java/android/vonnie/barrageview/MainActivity.java
@@ -0,0 +1,93 @@
+package android.vonnie.barrageview;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author LongpingZou
+ * @date 2019/3/29
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ArrayList<String> bubbles = new ArrayList<>();
+    private LinearLayout bubbleBarrageContainer;
+    private BubbleBarrage bubbleBarrage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        initBubbleBarrage();
+    }
+
+    private void initView() {
+        bubbleBarrageContainer = findViewById(R.id.bubble_barrage_container);
+
+        findViewById(R.id.append_more).setOnClickListener(this);
+        Button more = findViewById(R.id.append_more);
+        Shader shader = new LinearGradient(0, 0, 0, 20, Color.BLACK, Color.GRAY, Shader.TileMode.CLAMP);
+
+        more.getPaint().setShader(shader);
+        findViewById(R.id.insert_to_next).setOnClickListener(this);
+    }
+
+    private void initBubbleBarrage() {
+        bubbleBarrage = new BubbleBarrage()
+                .init(this, bubbleBarrageContainer, R.layout.layout_bubble_barrage_item)
+                .setIntervalTime(1000)
+                .setVisibleCount(3)
+                .setItemMargin(20)
+                .setOnBarrageLoadListener(new BubbleBarrage.OnBarrageLoadListener() {
+                    @Override
+                    public void loadBarrage(View view, List<String> data, int index) {
+                        view.findViewById(R.id.iv_head).setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.test_header));
+                        ((TextView) view.findViewById(R.id.tv_content)).setText(data.get(index));
+                    }
+                }).start(bubbles, 1000);
+    }
+
+    private void initData() {
+        for (int i = 0; i < 10; i++) {
+            bubbles.add("item --> " + i);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.append_more:
+                onAppendClick();
+                break;
+            case R.id.insert_to_next:
+                onInsertClick();
+                break;
+            default:
+        }
+    }
+
+
+    private void onInsertClick() {
+        bubbleBarrage.insertToNext("this is insert item");
+    }
+
+    private void onAppendClick() {
+        List<String> appends = new ArrayList<>();
+        for (int i = 100; i < 110; i++) {
+            appends.add("append item -->" + i);
+        }
+        bubbleBarrage.appendBarrages(appends);
+    }
+}
